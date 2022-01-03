412a096af6cf6d9d8bfbe49777c9c6c006a6d1c1
Shijie Li <lishijie1234@outlook.com>
2022-01-03 10:37:45 +0100

    Delete train.sh

--- train.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#block(name=pointconv-512, threads=2, memory=5000, gpus=1, hours=1000)
-python train.py --dataset data/single_scan --arch_cfg config/arch/PointConv-512.yaml --data_cfg config/labels/semantic-kitti.yaml --log logs/pointconv-512
